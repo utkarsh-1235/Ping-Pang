935277998dd4eafa5c75f4b2124f9d7d717db8f2
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2025-07-28 23:26:52 +0530

    Madifying home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Heading } from "./components/heading";
 import { MaxWidthWrapper } from "./components/max-width-wrapper";
+import { Check } from "lucide-react";
 
 export default function Home() {
   return (
@@ -15,8 +16,21 @@ export default function Home() {
           </Heading>
       </div>
       <p className="text-base/7">
-        PingPang is the easiest way to monitor your Saas. Get instant notifications for <span className="text-gray-700"> sales, new users, or any other event</span>sent directly to your discord
+        PingPang is the easiest way to monitor your Saas. Get instant notifications for {" "}<span className="text-gray-700"> sales, new users, or any other event</span>{" "}sent directly to your discord
       </p>
+      <ul className="space-y-2 text-base/7 text-gray-600 text-left flex flex-col items-center sm:items-start">
+      {[
+          "Real-time Discord alerts for critical events",
+          "Buy once, use forever",
+          "Track sales, new users, or any other event",
+      ].map((item, index) => (
+           <li key={index} className="flex gap-1.5 items-center text-center">
+            <Check className="size-5 shrink-0 text-brand-700"/>
+            {item}
+           </li>
+      ))}
+
+      </ul>
        </MaxWidthWrapper>
     </section>
     <section></section>
